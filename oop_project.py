e06d26d63dc86ec8472f83bd08add92962ce1447
Hanumanthareddy H <109300961+Hanumanthareddy884@users.noreply.github.com>
2023-11-22 08:58:54 +0530

    Saving Acct and Interest Rate calculate
    Saving Acct and Interest Rate calculate

--- oop_project.py
+++ oop_project.py
@@ -11,3 +11,13 @@ Reddy.deposit(500)
 Reddy.withdraw(811)
 
 Reddy.transfer(200,Mane)
+
+raghu = InterestRewardsAcct(1000,"raghu")
+raghu.getBalance()
+raghu.deposit(100)
+raghu.transfer(100,Reddy)
+
+Smith = SavingAcct(1000,"Smith")
+Smith.getBalance()
+Smith.deposit(20)
+Smith.transfer(100,Reddy)
